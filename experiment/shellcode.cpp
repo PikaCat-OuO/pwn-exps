d052159df85ac42af8c1a7d540eec6a84cd7d93e
PikaCat <760758491@qq.com>
2021-07-17 12:09:51 +0800

    更改了shellcode.cpp的实现

--- experiment/shellcode.cpp
+++ experiment/shellcode.cpp
@@ -1,23 +1,8 @@
-/*
-#include <sys/mman.h>
-#include <unistd.h>
-char shellcode[] = "\x48\xBB\x11\x2F\x62\x69\x6E\x2F\x73\x68\x48\xC1\xEB\x08\x53\x54\x5F\x48\x31\xC0\xB0\x3B\x48\x31\xF6\x48\x31\xD2\x0F\x05";
-int main() {
-  setuid(0);
-  mprotect((void*)(long(shellcode) & ~0xFFF), 0x1000, PROT_READ|PROT_WRITE|PROT_EXEC);
-  auto func = (void(*)())shellcode;
-  func();
-  return 0;
-}
-*/
-
 #include <sys/mman.h>
 #include <unistd.h>
 char shellcode[] = "\x48\x31\xF6\x48\x31\xD2\x48\x31\xC0\x04\x3B\x48\xBB\x11\x2F\x62\x69\x6E\x2F\x73\x68\x48\xC1\xEB\x08\x53\x54\x5F\x0F\x05";
 int main() {
-  setuid(0);
-  mprotect((void*)(long(shellcode) & ~0xFFF), 0x1000, PROT_READ | PROT_WRITE | PROT_EXEC);
+  mprotect((void*)(long(shellcode) & ~0xFFF), 0x1000, PROT_READ | PROT_EXEC);
   ((void(*)())shellcode)();
-  setuid(1000);
   return 0;
 }
